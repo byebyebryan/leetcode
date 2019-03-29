655281c39a4f23429527ad22dda7ff85832f8325
Bryan <byebyebryan@gmail.com>
2019-03-28 22:54:50 -0400

    solved

--- 127.word-ladder.cpp
+++ 127.word-ladder.cpp
@@ -14,153 +14,158 @@
  * Given two words (beginWord and endWord), and a dictionary's word list, find
  * the length of shortest transformation sequence from beginWord to endWord,
  * such that:
- * 
- * 
+ *
+ *
  * Only one letter can be changed at a time.
  * Each transformed word must exist in the word list. Note that beginWord is
  * not a transformed word.
- * 
- * 
+ *
+ *
  * Note:
- * 
- * 
+ *
+ *
  * Return 0 if there is no such transformation sequence.
  * All words have the same length.
  * All words contain only lowercase alphabetic characters.
  * You may assume no duplicates in the word list.
  * You may assume beginWord and endWord are non-empty and are not the same.
- * 
- * 
+ *
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input:
  * beginWord = "hit",
  * endWord = "cog",
  * wordList = ["hot","dot","dog","lot","log","cog"]
- * 
+ *
  * Output: 5
- * 
+ *
  * Explanation: As one shortest transformation is "hit" -> "hot" -> "dot" ->
  * "dog" -> "cog",
  * return its length 5.
- * 
- * 
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input:
  * beginWord = "hit"
  * endWord = "cog"
  * wordList = ["hot","dot","dog","lot","log"]
- * 
+ *
  * Output: 0
- * 
+ *
  * Explanation: The endWord "cog" is not in wordList, therefore no possible
  * transformation.
- * 
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
+ *
  */
 
-#include <vector>
-#include <string>
+// hot dot dog lot log cog
+//
+// oth otd ogd otl ogl ogc
+// tho tdo gdo tlo glo gco
+
+// h o t
+// d o t
+//     g
+// l o t
+//     g
+// c o g
+
+// o t h
+//     d
+//     l
+//   g d
+//     l
+//     c
+
+// t h o
+//   d o
+//   l o
+// g d o
+//   l o
+//   c o
+
 #include <queue>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 class Solution {
-public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        int source = -1;
-        int dest = -1;
+ public:
+  int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+    int source = -1;
+    int dest = -1;
 
-        int m = wordList.size();
+    int m = wordList.size();
 
-        for(int i=0; i<m; i++) {
-            if(wordList[i] == beginWord) {
-                source = i;
-            }
-            else if(wordList[i] == endWord) {
-                dest = i;
-            }
+    for (int i = 0; i < m; i++) {
+      if (wordList[i] == beginWord) {
+        source = i;
+      } else if (wordList[i] == endWord) {
+        dest = i;
+      }
 
-            if(source > -1 && dest > -1) break;
-        }
-        if(dest == -1) return 0;
+      if (source > -1 && dest > -1) break;
+    }
+    if (dest == -1) return 0;
 
-        if(check(beginWord, endWord)) return 1;
-        
-        bool connected[m][m];
+    if (getDist(beginWord, endWord) == 1) return 2;
 
-        for(int i=0; i<m; i++) {
-            for(int j=0; j<m; j++) {
-                connected[i][j] = false;
-            }
-        }
-
-        for(int i=0; i<m-1; i++) {
-            for(int j=i+1; j<m; j++) {
-                if(check(wordList[i], wordList[j])) {
-                    connected[i][j] = true;
-                    connected[j][i] = true;
-                }
-            }
-        }
-
-        queue<pair<int, int>> next;
-
-        bool checked[m];
-        for(int i=0; i<m; i++) {
-            checked[i] = false;
-        }
-
-        if(source > -1) {
-            checked[source] = true;
-            for(int i=0; i<m; i++) {
-                if(i != source && connected[source][i]) {
-                    next.push(make_pair(i, 1));
-                }
-            }
-        }
-        else {
-            for(int i=0; i<m; i++) {
-                if(check(beginWord, wordList[i])) {
-                    next.push(make_pair(i, 1));
-                }
-            }
-        }
-        
-
-        while (!next.empty()) {
-            auto node = next.front();
-            next.pop();
-
-            if(node.first == dest) return node.second;
-            checked[node.first] = true;
-
-            for(int i=0; i<m; i++) {
-                if( node.first != i && !checked[i] && connected[node.first][i]) {
-                    next.push(make_pair(i, node.second + 1));
-                }
-            }
-        }
-
-        return 0;
+    bool checked[m];
+    for (int i = 0; i < m; i++) {
+      checked[i] = false;
     }
 
-    bool check(string& a, string& b) {
-        int distance = 0;
+    if (source > -1) checked[source] = true;
 
-        for(int i=0; i< a.length(); i++) {
-            if(a[i] != b[i]) {
-                distance ++;
-                if(distance > 1) return false;
-            }
-        }
+    queue<pair<int, int>> next;
 
-        return distance == 1;
+    for (int i = 0; i < m; i++) {
+      if (checked[i]) continue;
+
+      if (getDist(beginWord, wordList[i]) == 1) {
+        next.push(make_pair(i, 1));
+        checked[i] = true;
+      }
     }
+
+    while (!next.empty()) {
+      int x = next.front().first;
+      int l = next.front().second;
+
+      next.pop();
+
+      if (x == dest) return l + 1;
+
+      for (int i = 0; i < m; i++) {
+        if (checked[i]) continue;
+
+        if (getDist(wordList[x], wordList[i]) == 1) {
+          next.push(make_pair(i, l + 1));
+          checked[i] = true;
+        }
+      }
+    }
+
+    return 0;
+  }
+
+  int getDist(string& a, string& b) {
+    int dist = 0;
+    for (int i = 0; i < a.length(); i++) {
+      if (a[i] != b[i]) {
+        dist++;
+        if (dist > 1) return dist;
+      }
+    }
+
+    return dist;
+  }
 };
--- 77.combinations.cpp
+++ 77.combinations.cpp
@@ -13,10 +13,10 @@
  *
  * Given two integers n and k, return all possible combinations of k numbers
  * out of 1 ... n.
- * 
+ *
  * Example:
- * 
- * 
+ *
+ *
  * Input: n = 4, k = 2
  * Output:
  * [
@@ -27,8 +27,8 @@
  * ⁠ [1,3],
  * ⁠ [1,4],
  * ]
- * 
- * 
+ *
+ *
  */
 
 #include <vector>
@@ -36,37 +36,36 @@
 using namespace std;
 
 class Solution {
-public:
-    vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>> res;
-        vector<int> runner;
+ public:
+  vector<vector<int>> combine(int n, int k) {
+    vector<vector<int>> res;
+    vector<int> runner;
 
+    assemble(res, runner, n, k);
+
+    return res;
+  }
+
+  void assemble(vector<vector<int>>& res, vector<int>& runner, int n, int k) {
+    if (runner.size() == k) {
+      res.push_back(runner);
+      return;
+    }
+
+    if (runner.empty()) {
+      for (int i = 1; i <= n - k + 1; i++) {
+        runner.push_back(i);
         assemble(res, runner, n, k);
-
-        return res;
+        runner.pop_back();
+      }
+    } else {
+      int m = runner[runner.size() - 1];
+      int d = k - runner.size() - 1;
+      for (int i = m + 1; i <= n - d; i++) {
+        runner.push_back(i);
+        assemble(res, runner, n, k);
+        runner.pop_back();
+      }
     }
-
-    void assemble(vector<vector<int>>& res, vector<int>& runner, int n, int k) {
-        if(runner.size() == k) {
-            res.push_back(runner);
-            return;
-        }
-
-        if(runner.empty()){
-            for(int i=1; i<=n-k+1;i++) {
-                runner.push_back(i);
-                assemble(res, runner, n, k);
-                runner.pop_back();
-            }
-        }
-        else {
-            int m = runner[runner.size() - 1];
-            int d = k - runner.size() - 1;
-            for(int i=m+1; i<=n-d; i++) {
-                runner.push_back(i);
-                assemble(res, runner, n, k);
-                runner.pop_back();
-            }
-        }
-    }
+  }
 };
